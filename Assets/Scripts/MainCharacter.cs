432faa997a29c10ed8575f24b38ad6661730b331
Daniel Gonzalez <gon20293@uvg.edu.gt>
2021-03-08 16:24:37 -0600

    Laboratorio 8 UVG 2021

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    public GameObject light;
+    public GameObject intermittentLight;
+    public GameObject ghost;
+    public AudioClip ghostScream;
+    public AudioClip lightSwitch;
+    public GameObject mjolnir;
+    public GameObject shield;
+    public GameObject helmet;
+
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        light.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (light)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                light.SetActive(!light.activeSelf);
+                audio.PlayOneShot(lightSwitch);
+            }
+        }
+
+        Vector3 pos = Input.mousePosition;
+        Ray myRay = Camera.main.ScreenPointToRay(pos);
+        RaycastHit rayInfo;
+        if (Physics.Raycast(myRay, out rayInfo, 3.0f))
+        {
+            if (rayInfo.collider.gameObject.CompareTag("Mjolnir"))
+            {
+                mjolnir.SetActive(true);
+            }
+            else if (rayInfo.collider.gameObject.CompareTag("Shield"))
+                shield.SetActive(true);
+            else if (rayInfo.collider.gameObject.CompareTag("Helmet"))
+                helmet.SetActive(true);
+            else
+            {
+                mjolnir.SetActive(false);
+                shield.SetActive(false);
+                helmet.SetActive(false);
+            }
+        }
+
+        Light lightInt = intermittentLight.GetComponent<Light>();
+        float algo = Mathf.Sin(Time.deltaTime * 100) * 2.0f;
+        print(algo);
+        lightInt.intensity = algo;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Portal"))
+        {
+            intermittentLight.SetActive(true);
+        }
+        else if(other.gameObject.CompareTag("Ghost"))
+            ghost.SetActive(false);
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Portal"))
+        {
+            intermittentLight.SetActive(false);
+            ghost.SetActive(true);
+            audio.PlayOneShot(ghostScream);
+        }
+
+
+    }
+}
